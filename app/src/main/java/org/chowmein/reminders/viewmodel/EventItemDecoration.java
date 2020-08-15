05f0122c7c767d8a50ca759b3dcebe104e52fb3f
Alex Chow <alex0091845@gmail.com>
2020-08-15 11:10:46 -0700

    EventItemDecoration styled

--- app/src/main/java/org/chowmein/reminders/viewmodel/EventItemDecoration.java
+++ app/src/main/java/org/chowmein/reminders/viewmodel/EventItemDecoration.java
@@ -7,9 +7,16 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+/**
+ * Used to style each RecyclerView item more properly.
+ */
 public class EventItemDecoration extends RecyclerView.ItemDecoration {
-    int spacing;
+    private int spacing;
 
+    /**
+     * Constructor that sets a custom spacing between items
+     * @param spacing spacing
+     */
     public EventItemDecoration(int spacing) {
         this.spacing = spacing;
     }
@@ -22,9 +29,13 @@ public class EventItemDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
         super.getItemOffsets(outRect, view, parent, state);
+
+        // only put a top spacing if the item is the first in the list
         if(parent.getChildAdapterPosition(view) == 0) {
             outRect.top = this.spacing;
         }
+
+        // default, common spacing for all items
         outRect.left = this.spacing;
         outRect.right = this.spacing;
         outRect.bottom = this.spacing;
